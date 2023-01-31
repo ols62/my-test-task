d65eaa84704a5526fe9431307e39d933d9876c1e
Oleg S <oleg.sl62@mail.ru>
2023-01-31 23:28:51 +0300

    feat: develop booking section

---
+++ src/js/checkbox-css.js
@@ -0,0 +1,14 @@
+let checkbox = document.getElementsByClassName('checkbox-css');
+for (let i = 0; i < checkbox.length; i++) {
+  let element = checkbox[i].getElementsByTagName('input');
+
+  element[0].addEventListener('change', function (event) {
+    if (event.target.checked) {
+      this.parentElement.style.background = '#9E7D43';
+      this.parentElement.style.color = '#FFFFFF';
+    } else {
+      this.parentElement.style.color = '#30261D';
+      this.parentElement.style.background = '#F6F5F3';
+    }
+  });
+}
